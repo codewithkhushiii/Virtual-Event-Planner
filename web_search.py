de65462fad20745b7e551b77e7582a5c2034a05f
codewithkhushiii <khushisharma972670@gmail.com>
2026-02-22 01:14:04 +0530

    Added Web-Search using Tavily

--- web_search.py
+++ web_search.py
@@ -1,11 +1,17 @@
 from dotenv import load_dotenv
+import os
 from langchain.agents import create_agent
 from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import InMemorySaver
 from dataclasses import dataclass
-from langchain.messages import HumanMessage
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import tool
+from typing import Dict, Any
+from tavily import TavilyClient
+
 
 load_dotenv()
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 checkpointer = InMemorySaver()
 
 model = init_chat_model("meta-llama/llama-4-scout-17b-16e-instruct",
@@ -16,11 +22,22 @@ model = init_chat_model("meta-llama/llama-4-scout-17b-16e-instruct",
 class Context:
     """Custom runtime context schema."""
     user_id: str
+    
+@tool
+def web_search(query: str) -> str:
+    """Search the web for information."""
+    response = tavily_client.search(query)
+    results = response.get("results", [])
+    return "\n\n".join(
+        f"Title: {r['title']}\nURL: {r['url']}\nContent: {r['content']}"
+        for r in results[:3]
+    )
 
 agent = create_agent(
     model=model,
     checkpointer=checkpointer,
-    context_schema=Context
+    context_schema=Context,
+    tools=[web_search]
 )
 
 config = {"configurable": {"thread_id": "1"}}
@@ -29,20 +46,13 @@ print("🤖 Agent is ready! Type 'exit' to stop.\n")
 
 # conversation loop
 while True:
-    
-    # take user input
     user_input = input("You: ")
-    
-    # exit condition
     if user_input.lower() == "exit":
         print("Agent: Goodbye 👋")
         break
 
-    question = HumanMessage(content = user_input )
-
     response = agent.invoke(
-        {"messages":[question]},
-        config=config,
-        context=Context(user_id="1")
+        {"messages": [HumanMessage(content=user_input)]},
+        config=config
     )
-    print("Agent:",response["messages"][-1].content)
+    print("Agent:", response["messages"][-1].content)
